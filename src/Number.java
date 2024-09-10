20524e39cf16c64f87f7793eb11048e4fbe1660f
sattique <sattique@ggc.edu>
2024-09-10 09:14:54 -0400

    fixed issue

--- src/Number.java
+++ src/Number.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Number {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter num1: ");
+        int num1 = input.nextInt();
+
+        System.out.println("Enter num2: ");
+        int num2 = input.nextInt();
+
+        System.out.println("Enter num3: ");
+        int num3 = input.nextInt();
+
+        int multi = num1 * num2 * num3;
+        System.out.println("OUTPUT of multiplication: num1 * num2 * num3 + multi");
+        System.out.print("multiplication: num1 * num2 * num3 = " + multi + "\n" );
+        System.out.println("OUTPUT of multiplication: num1 * num2 * num3 = + num1 * num2 * num3");
+        System.out.println("multiplication: num1 * num2 * num3 = " + num1 * num2 * num3 + "\n");
+        System.out.println("OUTPUT of multi");
+        System.out.println(multi + "\n");
+        System.out.println("OUTPUT of (multi * multi) ");
+        System.out.println(multi * multi + "\n");
+        System.out.println("OUTPUT of num1 * num2 * num3");
+        System.out.println(num1 * num2 * num3 );
+
+
+
+
+    }
 }
